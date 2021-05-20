26118037c2c3c336c0caf3ad43162594389271e2
f.sousa <ng.dev.sousa@outlook.com>
2021-05-20 12:56:40 -0300

    Add home link to Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
 import { Center, Image } from "@chakra-ui/react";
+import Link from "next/link";
 
 export function Header() {
   return (
-    <Center pt="7" pb="7">
-      <Image src="/logo.svg" w={180} h={45} />
-    </Center>
+    <Link href="/" passHref>
+      <Center as="a" pt="7" pb="7">
+        <Image src="/logo.svg" w={180} h={45} />
+      </Center>
+    </Link>
   );
 }
